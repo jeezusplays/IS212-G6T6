d35651d59731b5e48fde3698dd750032f21b0d42
Jeremy Goh <jeremygmc_@hotmail.com>
2023-09-18 15:53:37 +0800

    access right model as example
    did up accessrights.php as example model to follow, added pint.json to strict type all files

--- role-skill-match-app/app/Models/Access_Rights.php
+++ role-skill-match-app/app/Models/Access_Rights.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Access_Rights extends Model
 {
+    // creates Deleted At column in database, does not delete record
+    use SoftDeletes;
+    // links to factory for seeding
+    use HasFactory;
+
     // One-to-many relationship with `Permission_Rights` model
     public function rights(): HasMany
     {
@@ -18,4 +26,11 @@ class Access_Rights extends Model
     {
         return $this->hasMany(Staff::class, 'access_id');
     }
+    // define primary key
+    protected $primaryKey = 'access_id';
+
+    // define fields for mass assignment using object relational mapping
+    protected $fillable = [
+        'access_name',
+    ];
 }

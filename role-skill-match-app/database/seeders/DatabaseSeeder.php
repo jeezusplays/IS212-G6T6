dd8d1038d87e1577b5a5385911579c4a8cf3394c
jeezusplays <68149788+jeezusplays@users.noreply.github.com>
2023-09-19 15:48:40 +0800

    create seeders

--- role-skill-match-app/database/seeders/DatabaseSeeder.php
+++ role-skill-match-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Access_Rights;
+use App\Models\Application;
+use App\Models\Permission;
+use App\Models\Permission_Rights;
+use App\Models\Proficiency;
+use App\Models\Role_Skill;
+use App\Models\Staff;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +26,21 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            DepartmentSeeder::class,
+            CountrySeeder::class,
+            PermissionSeeder::class,
+            AccessRightsSeeder::class,
+            PermissionRightsSeeder::class,
+            ProficiencySeeder::class,
+            SkillSeeder::class,
+            StaffSeeder::class,
+            StaffSkillSeeder::class,
+            RoleSeeder::class,
+            ApplicationSeeder::class,
+            RoleManagerSeeder::class,
+            Role_Skill::class,
+        ]);
     }
 }

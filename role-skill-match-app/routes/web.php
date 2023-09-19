df70a3b2e3a81bd997b2c703f798d0cca8a1162b
kantkawkhin3 <140048767+kantkawkhin3@users.noreply.github.com>
2023-09-20 00:07:47 +0800

    update role UI
    with blade PHP and sample data

--- role-skill-match-app/routes/web.php
+++ role-skill-match-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,11 +17,69 @@ use App\Http\Controllers\RoleController;
 
 Route::get('/role-listings', [RoleController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/edit', function () {
+    return view('updateRole',[
+        'header' => "Update",
+
+         //this is the values to pass into view, possibly from backend
+         //retrieved values from role listing
+        'roleID' => "0123",
+        'title' => "Test Role",
+        'workArrangement' => "Part Time",
+        'department' => 1,
+        'hiring_managers' => ["Alvin Ho", "Amy", "John"],
+        'vacancy' => 1,
+        'deadline' => "2023-10-10",
+        'skills' =>  [1,2],
+        'description' => "Test Job", 
+        
+        'deptDDL' => [
+            [
+                "deptID" => 1,
+                "department" => "Sales"
+            ],
+            [
+                "deptID" => 2,
+                "department" => "IT"
+            ],
+            [
+                "deptID" => 3,
+                "department" => "HR"
+            ]
+            ],
+
+        'workArrangementDDL' => [
+            'Part Time', 'Full Time'
+        ],
+
+        'hiringManagerDDL' => [ //currently all staff
+            'Amy', 'John', 'Latrice'
+        ],
+
+        'skillsDDL' => [
+            [
+                "skillID" => 1,
+                "skill" => "Python"
+            ],
+            [
+                "skillID" => 2,
+                "skill" => "Excel"
+            ],
+            [
+                "skillID" => 3,
+                "skill" => "Management"
+            ],
+            [
+                "skillID" => 4,
+                "skill" => "Accounting"
+            ]
+        ]
+    ]);
 });
 
 
+<<<<<<< Updated upstream
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
@@ -28,3 +87,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+=======
+>>>>>>> Stashed changes

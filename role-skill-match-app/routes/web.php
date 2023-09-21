7d25ff2800c1f4f919e593d81e7ece893cf16507
hx240 <hx24000@gmail.com>
2023-09-21 16:43:06 +0800

    removed duplicates

--- role-skill-match-app/routes/web.php
+++ role-skill-match-app/routes/web.php
@@ -21,10 +21,6 @@ Route::get('/', function () {
 });
 
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

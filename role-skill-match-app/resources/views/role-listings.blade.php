1a635bd4c0d0f7507b0479681ffca54d515fdb90
hx240 <hx24000@gmail.com>
2023-09-25 23:07:50 +0800

    button default behaviour update - Checked filter status

--- role-skill-match-app/resources/views/role-listings.blade.php
+++ role-skill-match-app/resources/views/role-listings.blade.php
@@ -37,10 +37,10 @@
                 </ul>
             </div>
             <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown"
-                    aria-expanded="false">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                    data-bs-toggle="dropdown" aria-expanded="false">
                     {{-- Retrieve default HR staff name from database --}}
-                    {{$roles[4]['full_name']}} (HR Staff)
+                    {{ $roles[4]['full_name'] }} (HR Staff)
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <li><a class="dropdown-item" href="http://localhost:8000/role-listings">HR Staff</a></li>
@@ -56,9 +56,11 @@
         <div class="row justify-content-between">
             <div class="col-md-5 p-3 d-flex">
                 <div class="input-group">
-                    <input style="display:inline-block; position:relative" type="text" class="form-control" placeholder="Search for roles" id="myInput">
+                    <input style="display:inline-block; position:relative" type="text" class="form-control"
+                        placeholder="Search for roles" id="myInput">
                     <div class="input-group-append">
-                        <button class="btn btn-primary" type="button" onclick="searchJobs()" id="searchbutton">Search</button>
+                        <button class="btn btn-primary" type="button" onclick="searchJobs()"
+                            id="searchbutton">Search</button>
                     </div>
                 </div>
             </div>
@@ -66,101 +68,97 @@
             <!-- Filter by Status Dropdown Checkbox -->
             <div class="col-md-6 p-3 d-flex justify-content-end">
                 <div class="btn-group">
-                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-                        id="filterButton">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" id="filterButton">
                         Filter by Status
                     </button>
                     <div class="dropdown-menu" id="filterDropdown" style="max-height: 200px; overflow-y: auto;">
                         <label class="dropdown-item">
-                            <input type="checkbox" class="status-filter" value="Open"> Open
+                            <input type="checkbox" class="status-filter" value="Open" checked> Open
                         </label>
                         <label class="dropdown-item">
-                            <input type="checkbox" class="status-filter" value="Closed"> Closed
+                            <input type="checkbox" class="status-filter" value="Closed" checked> Closed
                         </label>
                     </div>
                 </div>
             </div>
         </div>
 
-        <h1 class = "my-3">Role Listings</h1>
+        <h1 class="my-3">Role Listings</h1>
 
         {{-- Loop through your role data and display each role in a card --}}
-            <div class="row" id="roleCardsContainer">
-                @foreach ($roles as $role)
-                <div class="col-xl-4 col-md-6 col-sm-12 role-card @if ($role['status'] == "Open") open-role @else closed-role @endif">
+        <div class="row" id="roleCardsContainer">
+            @foreach ($roles as $role)
+                <div
+                    class="col-xl-4 col-md-6 col-sm-12 role-card @if ($role['status'] == 'Open') open-role @else closed-role @endif">
                     <div class="card mb-3">
                         <h5 class="card-header card-title p-3">{{ $role['role'] }}</h5>
                         <div class="card-body">
-                            <p class="card-text">Applications received: <a href="#"><u>{{ $role['total_applications'] }}</u></a></p>
+                            <p class="card-text">Applications received: <a
+                                    href="#"><u>{{ $role['total_applications'] }}</u></a></p>
                             <p class="card-text">Creation Date: {{ $role['created_at'] }}</p>
                             <p class="card-text">Listed By: {{ $role['full_name'] }}</p>
                             <p class="card-text" id="card-status">
                                 Status:
-                                @if ($role['status'] == "Open")
-                                <span class="text-success">Open</span>
+                                @if ($role['status'] == 'Open')
+                                    <span class="text-success">Open</span>
                                 @else
-                                <span class="text-danger">Closed</span>
+                                    <span class="text-danger">Closed</span>
                                 @endif
                             </p>
                         </div>
                     </div>
                 </div>
-                @endforeach
+            @endforeach
         </div>
     </div>
 </body>
 
-    <script>
-        // Search bar functionality on enter key press
-        document.getElementById("myInput").addEventListener("keyup", function(event) {
-            event.preventDefault();
-            if (event.keyCode === 13) {
-                document.getElementById("searchbutton").click();
-            }
-        });
-
-        // Search bar functionality
-        function searchJobs() {
-            const input = document.getElementById("myInput");
-            const filter = input.value.toUpperCase();
-            const roleCards = document.querySelectorAll(".role-card");
-
-            roleCards.forEach(card => {
-                const jobTitle = card.querySelector(".card-title").textContent.toUpperCase();
-                if (jobTitle.indexOf(filter) > -1) {
-                    card.style.display = "";
-                } else {
-                    card.style.display = "none";
-                }
-            });
+<script>
+    // Search bar functionality on enter key press
+    document.getElementById("myInput").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("searchbutton").click();
         }
+    });
 
-        document.addEventListener("DOMContentLoaded", function() {
-            // Filter by Status functionality
-            const checkboxes = document.querySelectorAll(".status-filter");
-            checkboxes.forEach(checkbox => {
-                checkbox.addEventListener("change", applyFilters);
-            });
+    let currentSearchInput = "";
+
+    // Search bar functionality
+    function searchJobs() {
+        const input = document.getElementById("myInput");
+        currentSearchInput = input.value.toUpperCase();
+        applyFilters();
+    }
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // Filter by Status functionality
+        const checkboxes = document.querySelectorAll(".status-filter");
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener("change", applyFilters);
         });
+    });
 
-        function applyFilters() {
-            const selectedStatusFilters = Array.from(document.querySelectorAll(".status-filter:checked")).map(checkbox => checkbox.value);
+    function applyFilters() {
+        const selectedStatusFilters = Array.from(document.querySelectorAll(".status-filter:checked")).map(checkbox =>
+            checkbox.value);
 
-            if (selectedStatusFilters.length === 0) {
-                // Show all cards if no filters are selected
-                document.querySelectorAll(".role-card").forEach(card => card.style.display = "");
+        document.querySelectorAll(".role-card").forEach(card => {
+            const status = card.classList.contains("open-role") ? "Open" : "Closed";
+            const jobTitle = card.querySelector(".card-title").textContent.toUpperCase();
+
+            // Check if the card matches the search input and selected status filters
+            if (
+                jobTitle.indexOf(currentSearchInput) > -1 &&
+                (selectedStatusFilters.length === 0 || selectedStatusFilters.includes(status))
+            ) {
+                card.style.display = "";
             } else {
-                // Show cards based on selected filters
-                document.querySelectorAll(".role-card").forEach(card => {
-                    const status = card.classList.contains("open-role") ? "Open" : "Closed";
-                    if (selectedStatusFilters.includes(status)) {
-                        card.style.display = "";
-                    } else {
-                        card.style.display = "none";
-                    }
-                });
+                card.style.display = "none";
             }
-        }
-    </script>
+        });
+    }
+</script>
 
 </html>

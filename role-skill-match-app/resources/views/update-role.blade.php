837c406101b83603b2677cdee536c2fad5121f68
anthonyckho <anthony.ho.2021@scis.smu.edu.sg>
2023-10-05 23:35:45 +0800

    Update alert message

--- role-skill-match-app/resources/views/update-role.blade.php
+++ role-skill-match-app/resources/views/update-role.blade.php
@@ -281,8 +281,8 @@
                 })
             } else {
                 swal({
-                    title: "Role Updated",
-                    text: "Role has been updated successfully",
+                    title: "Role Created",
+                    text: "Role has been created successfully",
                     icon: "success",
                     // button: "Back",
                 });

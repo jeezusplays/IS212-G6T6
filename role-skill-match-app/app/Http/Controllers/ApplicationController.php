4e9b258688796089e53a6764abf158d25a970b55
Jeremy Goh <jeremygmc_@hotmail.com>
2023-10-06 01:12:17 +0800

    Update ApplicationController.php
    include backend checks and error messages for commented conditions

--- role-skill-match-app/app/Http/Controllers/ApplicationController.php
+++ role-skill-match-app/app/Http/Controllers/ApplicationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use App\Models\Role_Listing;
+use App\Models\Staff;
 use Illuminate\Http\Request;
 
 class ApplicationController extends Controller
@@ -11,6 +13,24 @@ class ApplicationController extends Controller
 
     public function store(Request $request)
     {
+        //check that application is for role that staff does not have
+        $staff_role = Staff::where('staff_id', $request->staff_id)->first()->role_id;
+        if ($staff_role == $request->role_id) {
+            return redirect()->back()->with('error', 'You already have this role!');
+        }
+
+        //check that listing has more than one vacancy slot
+        $listing = Role_Listing::where('listing_id', $request->listing_id)->first();
+        if ($listing->vacancy == 0) {
+            return redirect()->back()->with('error', 'This role has no vacancies!');
+        }
+
+        //check that less than 5 applications exist for staff
+        $existing_applications = Application::where('staff_id', $request->staff_id)->get();
+        if (count($existing_applications) >= 5) {
+            return redirect()->back()->with('error', 'You have reached the maximum number of applications!');
+        }
+
         $application = Application::firstOrCreate([
             'listing_id' => $request->listing_id,
             'staff_id' => $request->staff_id,

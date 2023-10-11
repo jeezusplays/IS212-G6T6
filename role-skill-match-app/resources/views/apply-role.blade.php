7dc75566d0be0b67d3418932f15822fd7ee4ecbb
anthonyckho <anthony.ho.2021@scis.smu.edu.sg>
2023-10-11 20:59:05 +0800

    Update navbar name

--- role-skill-match-app/resources/views/apply-role.blade.php
+++ role-skill-match-app/resources/views/apply-role.blade.php
@@ -62,7 +62,7 @@
             <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
                     data-bs-toggle="dropdown" aria-expanded="false">
-                    Staff Name (HR Staff)
+                    Li Ji Eun (Staff)
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     <li><a class="dropdown-item" href="http://localhost:8000/role-listings">HR Staff</a></li>

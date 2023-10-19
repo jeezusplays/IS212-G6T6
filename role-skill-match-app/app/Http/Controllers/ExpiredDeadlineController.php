4c944893b066de752f4ef399f0b19d93fbcc2231
dehou <dehou37@gmail.com>
2023-10-19 17:31:53 +0800

    combine where conditions together for updateStatusForExpiredDeadlines method

--- role-skill-match-app/app/Http/Controllers/ExpiredDeadlineController.php
+++ role-skill-match-app/app/Http/Controllers/ExpiredDeadlineController.php
@@ -7,15 +7,13 @@ use App\Models\Role_Listing;
 class ExpiredDeadlineController
 {
   public function updateStatusForExpiredDeadlines()
-  {
+{
     $currentDate = now()->toDateString();
 
-        // Update records where the deadline has passed today
+    // Update records where either condition is met
     Role_Listing::where('deadline', '<', $currentDate)
-      ->update(['status' => 2]);
-    Role_Listing::where('vacancy', '<', 1)
-      ->update(['status' => 2]);
-
-  }
+        ->orWhere('vacancy', '<', 1)
+        ->update(['status' => 2]);
+}
 }
 

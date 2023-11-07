494b8c15d5289fdb1ab349f9282d79ffa959bbeb
Jeremy Goh <jeremygmc_@hotmail.com>
2023-11-07 12:25:00 +0800

    clear homecontroller, fix apply role

--- role-skill-match-app/app/Http/Controllers/ApplicationController.php
+++ role-skill-match-app/app/Http/Controllers/ApplicationController.php
@@ -6,6 +6,7 @@ use App\Models\Application;
 use App\Models\Role_Listing;
 use App\Models\Staff;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ApplicationController extends Controller
 {
@@ -45,8 +46,17 @@ class ApplicationController extends Controller
 
         //check that skills for role listing match at least 1 skill for staff applying for this role
         $role_listing_skills = Role_Listing::where('listing_id', $listing_id)->first()->skills->pluck('skill_id');
-        $staff_skills = Staff::where('staff_id', $staff_id)->first()->skills;
-
+        $staff_skills = DB::table('staff_skill')
+            ->join('skill', 'staff_skill.skill_id', '=', 'skill.skill_id')
+            ->where('staff_id', $staff_id)
+            ->pluck('skill.skill_id');
+        $staff_skills = $staff_skills->mapWithKeys(function ($item, $key) {
+            return [$key => $item];
+        });
+        $matching_skills = $role_listing_skills->diff($staff_skills);
+        if (count($matching_skills) == count($role_listing_skills)) {
+            return redirect()->back()->with('error', 'You do not have the required skills for this role!');
+        }
 
         //redeclare existing applications to check if there is existing application
         $existing_applications = Application::where('staff_id', $staff_id)
@@ -57,13 +67,13 @@ class ApplicationController extends Controller
         //check if user already has the same exisitng application
         foreach ($existing_applications as $existing_application) {
             $listing_id = $existing_application->listing_id;
-            
+
             // Check if an application with the same listing_id, staff_id, and status 1, 2, or 3 exists
             $exists = Application::where('listing_id', $listing_id)
                 ->where('staff_id', $staff_id)
                 ->whereIn('status', [1, 2, 3])
                 ->exists();
-        
+
             if ($exists) {
                 return redirect()->back()->with('error', 'You have already applied for this role!');
             }
@@ -76,19 +86,11 @@ class ApplicationController extends Controller
             'application_date' => date('Y-m-d'),
         ]);
 
-        return redirect()->back()->with('success', 'Application created successfully!');
-        // $matching_skills = $role_listing_skills->intersect($staff_skills);
-        // if (count($matching_skills) == 0) {
-        //     return redirect()->back()->with('error', 'You do not have the required skills for this role!');
-        // }
+        if ($application->wasRecentlyCreated) {
+            return redirect()->back()->with('success', 'Application created successfully!');
+        } else {
+            return redirect()->back()->with('error', 'Application already exists!');
+        }
 
-   
-
-
-        // if ($application->wasRecentlyCreated) {
-        //     return back()->with('success', 'Application created successfully!');
-        // } else {
-        //     return back()->with('error', 'Application already exists!');
-        // }
     }
 }
